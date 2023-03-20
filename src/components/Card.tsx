ae24c6658b49a38444ce831c46031004e305d1f1
Ethan Morris <pirates.postbox-0t@icloud.com>
2023-03-19 21:25:13 -0500

    add files to github

---
+++ src/components/Card.tsx
@@ -0,0 +1,50 @@
+import Image from 'next/image';
+
+type CardProps = {
+	day: string;
+	meal: { name: string };
+	onLockToggle: () => void;
+	onRandomize: () => void;
+	locked: boolean;
+};
+
+const Card: React.FC<CardProps> = ({
+	day,
+	meal,
+	onLockToggle,
+	onRandomize,
+	locked,
+}) => {
+	return (
+		<div className="bg-white rounded-lg shadow-md p-4 m-2 flex flex-col justify-between">
+			<h2 className="text-dark-purple text-xl font-bold mb-2">{day}</h2>
+			<h3 className="text-purple-600 text-lg font-semibold mb-2">
+				{meal?.name ? meal.name : 'Nothing to see here.'}
+			</h3>
+			<div className="w-full h-36 relative">
+				<Image
+					className="w-full h-48 object-cover mb-2"
+					src={`https://source.unsplash.com/random/900x600/?${meal?.name}`}
+					alt={meal?.name}
+					fill
+				/>
+			</div>
+			<div className="flex justify-between">
+				<button
+					onClick={onLockToggle}
+					className="bg-teal text-white p-2 rounded"
+				>
+					{locked ? '🔓' : '🔒'}
+				</button>
+				<button
+					onClick={onRandomize}
+					className="bg-teal text-white p-2 rounded"
+				>
+					🔄
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default Card;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx}',
+		'./src/components/**/*.{js,ts,jsx,tsx}',
+		'./src/app/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
+					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+			backgroundColor: {
+				'dark-purple': '#5a3d5c',
+				teal: '#46B1C9',
+			},
+			textColor: {
+				'dark-purple': '#5a3d5c',
+				teal: '#46B1C9',
+			},
+		},
+	},
+	plugins: [],
+};
